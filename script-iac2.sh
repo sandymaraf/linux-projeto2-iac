bdfeecb6290cdddfd568d25602411fb1b8fb52dc
sandymaraf <sandymaraf11@gmail.com>
2023-04-23 01:32:38 +0000

    Apache v1

---
+++ script-iac2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Atualizando o servidor e baixando programas necessários"
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e copiando os arquivos da aplicação"
+
+cd /tmp
+apt-get install wget
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
